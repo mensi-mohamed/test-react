d999ec6af273c8d3badb6e5b29020a29f9d85183
mensi_mohamed <mensi.mohamed@hotmail.com>
2017-04-11 10:18:20 +0200

    add reset button

--- CowClicker.js
+++ CowClicker.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import { render } from 'react-dom'
-var CowClicker = React.createClass({
-    getInitialState: function() {
-        return {
-            clicks: 0
-        };
-    },
+        import { render } from 'react-dom'
+        var CowClicker = React.createClass({
+            getInitialState: function () {
+                return {
+                    clicks: 0
+                };
+            },
 
-    onCowClick: function(evt) {
-        this.setState({clicks: this.state.clicks + 1});
-    },
-    render() {
-        return (
-            <div>
-                <div>Clicks: {this.state.clicks}</div>
-                <img src="http://s3.bypaulshen.com/buildwithreact/cow.png" onClick={this.onCowClick}/>
-            </div>
-        );
-    }
-});
+            onCowClick: function (evt) {
+                this.setState({clicks: this.state.clicks + 1});
+            },
+            onCowReset: function (evt) {
+                this.setState({clicks: 0});
+            },
+            render() {
+                return (
+                        <div>
+                            <div>Clicks: {this.state.clicks}</div>
+                            <img src="http://s3.bypaulshen.com/buildwithreact/cow.png" 
+                                 onClick={this.onCowClick}/>
+                            <div>
+                                <button onClick={this.onCowReset}>reset</button>
+                            </div>
+                        
+                                                
+                                        
+                        </div>
+                        );
+            }
+        });
 export default CowClicker;
